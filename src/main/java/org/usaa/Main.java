da5aafe4e5d67369137cefbeae69e0b22cc0ad4d
SaiCharanAgapati <saicharanagapati1999@gmail.com>
2025-03-16 13:48:46 +0530

    FirstCommit

---
+++ src/main/java/org/usaa/Main.java
@@ -0,0 +1,15 @@
+package org.usaa;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        String s="Charan";
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
